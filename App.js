a20f7f446663ebddedff2730ad342b0450528a9c
brianpompey <brianthescholar@gmail.com>
2021-11-09 12:27:33 -0500

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
+import DateTime from './components/DateTime';
+
+const img = require("./assets/weather-background.jpg")
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={img} style={styles.imgBackground}>
+        <DateTime />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +18,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  imgBackground: {
+    flex: 1, 
+    resizeMode: "cover", 
+    justifyContent: "center",
+  }
 });
---
+++ components/DateTime.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const WeatherItem = ({title, value, unit}) => {
+    return(
+        <View>
+            <Text>{title}</Text>
+            <Text>{value}{unit}</Text>
+        </View>
+    )
+}
+
+const DateTime = () => {
+    return(
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <Text style={styles.heading}>12:30am</Text>
+                </View>
+                <View>
+                    <Text style={styles.subheading}>Tuesday, November 9</Text>
+                </View>
+                <View>
+                    <WeatherItem title="Humidity" value="79" unit="%"/>
+                    <WeatherItem title="Pressure" value="1000" unit="hPA"/>
+                    <WeatherItem title="Sunrise" value="06:00" unit="am"/>
+                    <WeatherItem title="Sunset" value="06:30" unit="pm"/>
+
+                </View>
+            </View>
+            <View style={styles.rightAlign}>
+                <Text style={styles.location}>New York, NY</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1.5,
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    heading: {
+        fontSize: 45,
+        color: 'white',
+        fontWeight: '100'
+    },
+    subheading: {
+        fontSize: 25,
+        color: '#eee',
+        fontWeight: '300'
+    },
+    rightAlign: {
+        textAlign:'right',
+    },
+    location: {
+        fontSize:16,
+        color: 'white',
+    }
+})
+
+export default DateTime;
